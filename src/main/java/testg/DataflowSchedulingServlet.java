fcffc02e53d40283078b39931e00f1f18a1861d7
uday <uday@DESKTOP-PU1720C>
2018-05-22 21:18:18 +0530

    #1

---
+++ src/main/java/testg/DataflowSchedulingServlet.java
@@ -0,0 +1,21 @@
+package testg;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DataflowSchedulingServlet
+ */
+@WebServlet(name = "dataflowscheduler", value = "/dataflows")
+public class DataflowSchedulingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MinimalWordCount.run();
+	}
+
+}
